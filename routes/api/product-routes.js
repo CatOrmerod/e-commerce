966a2d92ccb869701a483dfcb4e80b42c6422163
Cat Ormerod <catormerod@gmail.com>
2021-05-26 10:06:11 +1000

    Updated product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,13 +9,13 @@ router.get('/', async (req, res) => {
     const productData = await Product.findAll({
       include: [
         {
-        model: Category,
-        attributes: ['id', 'category_name']
+          model: Category,
+          attributes: ['id', 'category_name']
         },
         {
           model: Tag,
           attributes: ['id', 'tag_name']
-          },
+        },
       ]
     });
     res.status(200).json(productData);
@@ -33,13 +33,13 @@ router.get('/:id', async (req, res) => {
       // JOIN with travellers, using the Trip through table
       include: [
         {
-        model: Category,
-        attributes: ['id', 'category_name']
+          model: Category,
+          attributes: ['id', 'category_name']
         },
         {
           model: Tag,
           attributes: ['id', 'tag_name']
-          },
+        },
       ]
     });
 
@@ -66,7 +66,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
